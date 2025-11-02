9d3d7050d186a43574bcd4ff6864c3dbb15c1d28
Brylle <bryanpalay119@gmail.com>
2025-11-02 20:14:13 +0800

    lib

---
+++ lib/server/server.ts
@@ -0,0 +1,11 @@
+// This file can be used for server-side authentication utilities
+// Example usage:
+// import { auth } from "@/lib/auth-session/auth";
+// import { headers } from "next/headers";
+//
+// const someAuthenticatedAction = async () => {
+//   "use server";
+//   const session = await auth.api.getSession({
+//     headers: await headers(),
+//   });
+// };
